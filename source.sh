24418145519b75be02c06bf209c927427f91c3ff
bows7ring <60060864+bows7ring@users.noreply.github.com>
2023-02-16 04:31:37 +0000

    build changes

---
+++ source.sh
@@ -0,0 +1,3 @@
+export RISCV=/workspaces/keystone/riscv64
+export PATH=$RISCV/bin:$PATH
+export KEYSTONE_SDK_DIR=/workspaces/keystone/sdk/build64
---
+++ zzp-build-note.md
@@ -0,0 +1,9 @@
+# 修改项
+## patch
+patch那行加个|| true
+
+## buildroot
+不然ncruse下载不下来
+用最新master的话工具链版本不对
+git checkout 2022.11
+git pull
